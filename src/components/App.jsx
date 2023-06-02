17411da4b6ed2acc2d34439d3a2f211dd78d5061
Dmytro Kozakov <dimakozakov1999@gmail.com>
2023-06-02 17:03:49 +0300

    Adds the option to save contacts to localeStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,17 +7,28 @@ import { Container } from './Container.styled';
 import { ContactFormSection } from './ContactForm/ContactFormSection.styled';
 import { ContactListSection } from './ContactList/ContactListSection.styled';
 
+const KEY = 'contacts';
+
 const INITIAL_STATE = {
-  contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  contacts: [],
   filter: '',
 };
 export class App extends Component {
   state = { ...INITIAL_STATE };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem(KEY));
+
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem(KEY, JSON.stringify(this.state.contacts));
+    }
+  }
 
   changeFilter = e => {
     this.setState({ filter: e.target.value.toLowerCase() });
